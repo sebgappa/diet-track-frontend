3f763363f7c22734e81a39b3329ba0333e4a51f7
UKGC <seb.gappa@gmail.com>
2021-04-24 21:32:12 +0100

    git ignore didn't work

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -12,12 +12,12 @@ export const environment = {
     allowedList: [`${apiUri}/*`],
   },
   firebase: {
-    apiKey: "AIzaSyA3Z49ooRwRR5bSagqbslj2pCEwmivNRsk",
-    authDomain: "diettrack-4850c.firebaseapp.com",
-    projectId: "diettrack-4850c",
-    storageBucket: "diettrack-4850c.appspot.com",
-    messagingSenderId: "756064667679",
-    appId: "1:756064667679:web:953118798dbe6f2c682d2a",
-    measurementId: "G-VH263DL0HY"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   }
 };
